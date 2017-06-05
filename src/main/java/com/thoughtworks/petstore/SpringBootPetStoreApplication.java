caa69ed4cda8d1b9673c4fdbf6195e7b7bcb3a0f
aisensiy <aisensiy@163.com>
2017-06-05 17:11:45 +0800

    init commit

---
+++ src/main/java/com/thoughtworks/petstore/SpringBootPetStoreApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thoughtworks/petstore/SpringBootPetStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.petstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPetStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
