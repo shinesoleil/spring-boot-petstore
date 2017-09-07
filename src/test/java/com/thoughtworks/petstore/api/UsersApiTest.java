0f1584f29e1c3912b9aa4e6b555a8cef9461d160
hpan <hao_pan@outlook.com>
2017-09-07 14:19:01 +0800

    add one failing test

--- src/test/java/com/thoughtworks/petstore/api/UsersApiTest.java
+++ src/test/java/com/thoughtworks/petstore/api/UsersApiTest.java
@@ -66,7 +66,7 @@ public class UsersApiTest {
             .when()
             .post("/users")
             .then()
-            .statusCode(400) //400
+            .statusCode(401) //400
             .body("fieldErrors[0].field", equalTo("email"));
     }
 
